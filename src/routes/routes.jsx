40d01bfa948af8b4fd7305f8d6c386090e4357b8
marcosm <marcos.martilotta@gmail.com>
2023-03-30 20:42:20 -0300

    Agregué favicon

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout";
+import Home from "../pages/Home/Home.jsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
+      { path: "/", element: <Home /> },
       { path: "/products/:id", element: <span>Category</span> },
       { path: "/product/:id", element: <span>Product</span> },
     ],
