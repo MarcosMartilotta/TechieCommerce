c1e7dd19eb83fc7a8500eac30e37dbe07b6681c5
marcosm <marcos.martilotta@gmail.com>
2023-04-05 20:39:59 -0300

    Slider imagenes

---
+++ src/pages/Home/components/Arrows/Arrows.jsx
@@ -0,0 +1,15 @@
+import './arrows.scss'
+const Arrows = ({ nextSlide, prevSlide }) => {
+  return (
+    <>
+      <div className="arrow-left" onClick={prevSlide}>
+        {'<'}
+      </div>
+      <div className="arrow-right" onClick={nextSlide}>
+        {'>'}
+      </div>
+    </>
+  )
+}
+
+export default Arrows
--- src/pages/Home/components/Slider/Slider.jsx
+++ src/pages/Home/components/Slider/Slider.jsx
@@ -1,4 +1,6 @@
 import './slider.scss'
+import Arrows from '../Arrows/Arrows'
+import { useState } from 'react'
 
 const imgOne =
   'https://images.pexels.com/photos/9072343/pexels-photo-9072343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
@@ -10,23 +12,33 @@ const imgFour =
   'https://images.pexels.com/photos/3721941/pexels-photo-3721941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 3 ? 0 : currentSlide + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 3 : currentSlide - 1)
+  }
+
   return (
-    <div className="slider-container">
-      <figure>
+    <div className="slider">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <img
           src={imgOne}
           alt="Imagen de teclado y manos jugando counter strike"
         />
-      </figure>
-      <figure>
         <img src={imgTwo} alt="" />
-      </figure>
-      <figure>
         <img src={imgThre} alt="" />
-      </figure>
-      <figure>
         <img src={imgFour} alt="" />
-      </figure>
+      </div>
+      <div className="arrows">
+        <Arrows nextSlide={nextSlide} prevSlide={prevSlide} />
+      </div>
     </div>
   )
 }
