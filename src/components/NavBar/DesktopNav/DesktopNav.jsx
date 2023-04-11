1946b1118d1bb9371d3d7753fa05420f43082a02
marcosm <marcos.martilotta@gmail.com>
2023-04-11 20:54:24 -0300

    Hot fix, productos en nav bar

--- src/components/NavBar/DesktopNav/DesktopNav.jsx
+++ src/components/NavBar/DesktopNav/DesktopNav.jsx
@@ -1,8 +1,10 @@
-import "./desktopNav.scss";
-
+import './desktopNav.scss';
+import { useNavigate } from 'react-router-dom';
 const DesktopNav = () => {
+  const navigate = useNavigate();
   return (
     <div className="categories-container">
+      <h2 onClick={() => navigate('/products')}>Productos</h2>
       <span>Teclados</span>
       <span>Mouses</span>
       <span>Auriculares</span>
